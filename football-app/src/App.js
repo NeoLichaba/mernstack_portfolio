1d2ef87c7cb66e111fec94a2a52d0d2ff19023b1
Neo Lichaba <neolicham@gmail.com>
2022-05-19 11:49:36 +0200

    football app first commit

---
+++ football-app/src/App.js
@@ -0,0 +1,18 @@
+
+import React, { Component }  from 'react';
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
