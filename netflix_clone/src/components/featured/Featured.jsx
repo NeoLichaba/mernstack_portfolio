90b36c0ef81b6ab6692b7421f8575356e15504f1
Neo Lichaba <neolicham@gmail.com>
2022-05-25 23:04:35 +0200

    netflix clone first commit

---
+++ netflix_clone/src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import "/.featured.scss";
+
+export default function Featured() {
+  return (
+    <div classname="featured">
+       <img 
+       src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="">
+       </img> 
+
+    </div>
+  )
+}
