dd2be95a974abcd86f8e5240ed0228098acde96a
Neo Lichaba <neolicham@gmail.com>
2022-05-31 22:54:41 +0200

    Movie CRUD operations

--- netflix_clone/api/routes/users.js
+++ netflix_clone/api/routes/users.js
@@ -7,15 +7,15 @@ const verify = require('../verifyToken');
 
 router.put("/:id", verify, async (req, res) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
-      if (req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(
-          req.body.password,
-          process.env.SECRET_KEY
-        ).toString();
+        if (req.body.password) {
+            req.body.password = CryptoJS.AES.encrypt(
+                req.body.password,
+                process.env.SECRET_KEY
+            ).toString();
         }
         try {
-            const updatedUser = await User.findByIdandUpdate(req.params.id, { 
-                $set: req.body, 
+            const updatedUser = await User.findByIdandUpdate(req.params.id, {
+                $set: req.body,
             });
             res.status(200).json(updatedUser);
         } catch (err) {
@@ -27,7 +27,86 @@ router.put("/:id", verify, async (req, res) => {
     }
 });
 //DELETE
+
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            await User.findByIdandDelete(req.params.id);
+            res.status(200).json("User has been deleted...");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+
+    } else {
+        res.status(403).json("You can only delete your account")
+    }
+});
+
 //GET
+
+router.get("/find/:id", verify, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...info } = user._doc;
+        res.status(200).json(info);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 //GET ALL
+
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().sort({ id: -1 }).limit(10) : await User.find()
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all users!");
+    }
+});
+
 //GET USER STATS
+
+router.get('/stats', async (req, res) => {
+    const today = new Date();
+    const lastYear = today.setFullYear(today.getFullYear() - 1);
+
+    const monthsArray = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    try {
+        const data = await User.aggregate([
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: 1 }
+                },
+            },
+        ]);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 module.exports = router;
