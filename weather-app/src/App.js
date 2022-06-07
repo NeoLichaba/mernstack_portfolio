5ee59c06cf79a600235be5850e1325ce816723c6
Neo Lichaba <neolicham@gmail.com>
2022-06-07 15:37:50 +0200

    added date and time feature to weather app

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,8 +8,10 @@ function App() {
   const [icon, setIcon] = useState("")
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=cb90310f52e8a567778a8e7b68e57902`
+
   
-  
+ 
+
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
@@ -19,6 +21,28 @@ function App() {
       setLocation('')
     }
   }
+
+  //DATE
+
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = d.toLocaleString("default", {month: "long"});
+  let day = d.toLocaleString("default", {weekday: "long"});
+
+
+  //TIME
+
+  let time = d.toLocaleString([], {
+    hour: '2-digit',
+    minute: '2-digit',    
+  });
+
+  let sunrise = (data.sys.sunrise).toLocaleString([], {
+    hour: '2-digit',
+    minute: '2-digit',    
+  });
+
 
   return (
     <div className="app">
@@ -37,16 +61,34 @@ function App() {
           </div>
           <div className="temp">
             {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            <br></br>
           </div>
           <div className="country">
-            {data.main ? <h2>{data.sys.country}</h2> : null}                           
-            </div>
+            {data.main ? <h2>{data.sys.country}</h2> : null}
+            <br></br>
+          </div>
+          <div className="date">
+            {data.main ? <p>{day}, {date} {month} {year}, 
+            <br></br>
+            {time} </p>: null}
+          </div>
           <div className="description">
             {data.weather ? <h2>{data.weather[0].description}</h2> : null}
+          </div>
+          <div className="iconId">
+            {data.weather ? <h2>{data.weather[0].icon}</h2> : null}
           </div>
         </div>
         {data.name !== undefined &&
           <div className="bottom">
+            <div className="sunrise">
+            {data.sys ? <h2>{sunrise}</h2> : null}
+              <p>Sunrise</p>
+            </div>
+            <div className="sunset">
+              {data.sys ? <h2>{data.sys.sunset}</h2> : null}
+              <p>Sunset</p>
+            </div>
             <div className="feels">
               {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
               <p>Feels Like</p>
@@ -57,7 +99,7 @@ function App() {
             </div>
             <div className="wind">
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} KM/H</p> : null}
-              <p>Wind Speed</p>                                              
+              <p>Wind Speed</p>
             </div>
           </div>
         }
