74b6a33b052e96699457032b03b6207633ca7cb8
Neo Lichaba <neolicham@gmail.com>
2022-06-11 11:12:34 +0200

    final portfolio gallery uploaded

--- portfolio_gallery/src/App.js
+++ portfolio_gallery/src/App.js
@@ -1,25 +1,12 @@
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio_gallery/Gallery";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+function App() {  
+    
+      return (
+       <Gallery />
+    );
+    }
+  
 export default App;
--- portfolio_gallery/src/portfolio_gallery/Data.js
+++ portfolio_gallery/src/portfolio_gallery/Data.js
@@ -2,120 +2,120 @@ const data = [
     {
         id: 1,
         image: "./images/html5-logo.png",
-        title: "HTML Tutoril",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        title: "HTML Tutorial",
+        description: "HTML, short for hypertext markup language, a formatting system for displaying material retrieved over the Internet.",
         category: "frontend",
 
     },
     {
         id: 2,
-        image: "./images/css.jpg",
+        image: "./images/css_tutorial.png",
         title: "CSS Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media  ",
         category: "frontend",
 
     },
     {
         id: 3,
-        image: "./images/javascript.jpg",
+        image: "./images/javascript-logo.png",
         title: "JS Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "JavaScript (aka JS) is a lightweight, interpreted, object-oriented language best known as the scripting language for Web pages. ",
         category: "frontend",
 
     },
     {
         id: 4,
-        image: "./images/react.jpg",
+        image: "./images/react_tutorial.png",
         title: "React Js Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "React is a declarative, efficient, and flexible JavaScript library for building user interfaces. ",
         category: "frontend",
 
     },
     {
         id: 5,
-        image: "./images/bootstrap.jpg",
+        image: "./images/bootstrap-logo.png",
         title: "Bootstrap Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "Bootstrap is a free and open source front end development framework for the creation of websites and web apps. ",
         category: "frontend",
 
     },
     {
         id: 6,
-        image: "./images/php.jpg",
+        image: "./images/php-logo-1.png",
         title: "PHP Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "It is used to manage dynamic content, databases, session tracking, even build entire e-commerce sites. ",
         category: "backend",
 
     },
     {
         id: 7,
-        image: "./images/node-js.jpg",
+        image: "./images/nodejs-logo.png",
         title: "Node.js Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "Node is useful for developing applications that require a persistent connection from the browser to the server and is often used for real-time applications ",
         category: "backend",
 
     },
     {
         id: 8,
-        image: "./images/asp.jpg",
+        image: "./images/raspberry-pi-logo.png",
         title: "ASP Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "ASP.NET is an open-source, server-side web-application framework designed for web development to produce dynamic web pages. ",
         category: "backend",
 
     },
     {
         id: 9,
-        image: "./images/sql.jpg",
+        image: "./images/postgresql-inc-logo.png",
         title: "SQL Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "SQL stands for Structured Query Language which is basically a language used by databases. ",
         category: "database",
 
     },
     {
         id: 10,
-        image: "./images/mongodb.jpg",
+        image: "./images/mongodb-logo.png",
         title: "Mongo DB Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "MongoDB is an open source NoSQL database management program. ",
         category: "database",
 
     },
     {
         id: 11,
-        image: "./images/c.jpg",
+        image: "./images/c1-logo.png",
         title: "C Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "Designed to be compiled to provide low-level access to memory and language constructs that map efficiently to machine instructions, all with minimal runtime support. ",
         category: "programming",
 
     },
     {
         id: 12,
-        image: "./images/c-plus.jpg",
+        image: "./images/c-logo.png",
         title: "C++ Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "C++ is an object-oriented programming language which gives a clear structure to programs and allows code to be reused, lowering development costs.. ",
         category: "programming",
 
     },
     {
         id: 13,
-        image: "./images/java.jpg",
+        image: "./images/java-logo-3.png",
         title: "Java Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: " An object-oriented programming language that produces software for multiple platforms. ",
         category: "programming",
 
     },
     {
         id: 14,
-        image: "./images/python.jpg",
+        image: "./images/python-logo.png",
         title: "Python Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        description: "Python is a computer programming language often used to build websites and software, automate tasks, and conduct data analysis. ",
         category: "programming",
 
     },
     {
         id: 15,
-        image: "./images/go.jpg",
+        image: "./images/go-logo.png",
         title: "Go Tutorial",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        description: "Go (also called Golang or Go language) is an open source programming language used for general purpose.",
         category: "programming",
 
     },
