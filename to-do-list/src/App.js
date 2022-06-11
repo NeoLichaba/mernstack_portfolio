706f8a80e4ca68838b1d3a6254bb98dad7c062bc
Neo Lichaba <neolicham@gmail.com>
2022-06-11 21:57:24 +0200

    todo list app

---
+++ to-do-list/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TodoApp from "./todo/todoApp.js";
+
+
+class App extends React.Component {
+
+  render(){
+
+  return (
+    <TodoApp />
+    
+  );
+
+  }
+}
+export default App;
+
+
