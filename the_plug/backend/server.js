5067aefb905b8dcf97d200861ba323df7a03e696
Neo Lichaba <neolicham@gmail.com>
2022-06-17 20:01:09 +0200

    serving and fetching data from express complete

--- the_plug/backend/server.js
+++ the_plug/backend/server.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express';
+import { config } from 'dotenv';
+import products from './data/products.js';
 
-const app = express()
+config();
+
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+  res.send('API is running...');
+});
 
 app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+  res.json(products);
+});
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+  const product = find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000;
 
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
