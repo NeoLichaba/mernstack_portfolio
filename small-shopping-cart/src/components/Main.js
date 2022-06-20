49f02222d87fc03de1aa197c646d932774e5c8a7
Neo Lichaba <neolicham@gmail.com>
2022-06-20 17:56:04 +0200

    remove from cart implemented

--- small-shopping-cart/src/components/Main.js
+++ small-shopping-cart/src/components/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Product from './Product';
 
 export default function Main(props) {
-  const { products } = props;
+  const { products, onAdd } = props;
   return (
-    <main className="block col-2">
+    <main className='block col-2'>
       <h2>Products</h2>
-      <div className="row">
+      <div className='row'>
         {products.map((product) => (
-          <Product key={product.id} product={product}></Product>
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
         ))}
       </div>
     </main>
--- small-shopping-cart/src/components/Product.js
+++ small-shopping-cart/src/components/Product.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default function Product(props) {
-    const {product} = props;
+    const {product, onAdd} = props;
   return (
     <div>
         <img className='small' src={product.image} alt={product.name}></img>
         <h3>{product.name}</h3>
         <div>${product.price}</div>
         <div>
-            <button>Add To Cart</button>
+            <button onClick={() => onAdd(product)}>Add To Cart</button>
         </div>
     </div>
   )
