cc910621c57af3f312e24a8172bf77eb65d07a49
Neo Lichaba <neolicham@gmail.com>
2022-06-23 00:05:57 +0200

    final temp control app

--- temp-control/src/App.js
+++ temp-control/src/App.js
@@ -1,9 +1,30 @@
-
 import React, { useState} from 'react';
 
 const App = () => {
   const [temperatureValue, setTemperatureValue] = useState(10);
   const [temperatureColor, setTemperatureColor] = useState("cold");
+  
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return; //if temperatureValue is equal to 30, then return -no further action taken
+    const newTemperature = temperatureValue + 1;
+
+    if (newTemperature >= 15) { //if the temperature value is greater or equal to 15, then render/call the setTemperatureColor function
+      setTemperatureColor('hot');
+    }
+    setTemperatureValue(newTemperature);
+
+;
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) 
+    return; 
+    const newTemperature = temperatureValue - 1;
+
+    if (newTemperature < 15) { //if the temperature value is less than 15, then render/call the setTemperatureColor function
+      setTemperatureColor('cold');
+    }
+    setTemperatureValue(newTemperature);
+  };
 
   return (
     <div className='app-container'>
@@ -11,11 +32,12 @@ const App = () => {
         <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°C</div>   
         </div>
         <div className='button-container'>
-          <button onClick ={() => setTemperatureValue(temperatureValue +1)}>+</button>
-          <button onClick ={() => setTemperatureValue(temperatureValue-1)}>-</button>
+          <button onClick ={() => increaseTemperature()}>+</button>
+          <button onClick ={() => decreaseTemperature()}>-</button>
         </div>
     </div>
   )
 
+}
 }
 export default App;
